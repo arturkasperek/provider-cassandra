b49fccbe03de45ff823754e0ba137e9fc97184ae
Mona Lisa <arturmiks@gmail.com>
2025-01-02 22:12:41 +0100

    Test changes

---
+++ internal/clients/cassandra/cassandra_mock.go
@@ -0,0 +1,60 @@
+package cassandra
+
+import (
+	"context"
+
+	"github.com/gocql/gocql"
+
+	xpv1 "github.com/crossplane/crossplane-runtime/apis/common/v1"
+	"github.com/crossplane/crossplane-runtime/pkg/reconciler/managed"
+)
+
+type MockDB struct {
+	ExecFunc                func(ctx context.Context, query string, args ...interface{}) error
+	QueryFunc               func(ctx context.Context, query string, args ...interface{}) (*gocql.Iter, error)
+	ScanFunc                func(iter *gocql.Iter, dest ...interface{}) bool
+	CloseFunc               func()
+	GetConnectionDetailsFunc func(username, password string) managed.ConnectionDetails
+}
+
+// Exec executes a CQL statement.
+func (m *MockDB) Exec(ctx context.Context, query string, args ...interface{}) error {
+	if m.ExecFunc != nil {
+		return m.ExecFunc(ctx, query, args...)
+	}
+	return nil
+}
+
+// Query performs a query and returns an iterator for the results.
+func (m *MockDB) Query(ctx context.Context, query string, args ...interface{}) (*gocql.Iter, error) {
+	if m.QueryFunc != nil {
+		return m.QueryFunc(ctx, query, args...)
+	}
+	return nil, nil
+}
+
+// Scan performs scanning of an iterator.
+func (m *MockDB) Scan(iter *gocql.Iter, dest ...interface{}) bool {
+	if m.ScanFunc != nil {
+		return m.ScanFunc(iter, dest...)
+	}
+	return false
+}
+
+// Close closes the Cassandra session.
+func (m *MockDB) Close() {
+	if m.CloseFunc != nil {
+		m.CloseFunc()
+	}
+}
+
+// GetConnectionDetails returns the connection details for a user of this DB.
+func (m *MockDB) GetConnectionDetails(username, password string) managed.ConnectionDetails {
+	if m.GetConnectionDetailsFunc != nil {
+		return m.GetConnectionDetailsFunc(username, password)
+	}
+	return managed.ConnectionDetails{
+		xpv1.ResourceCredentialsSecretUserKey:     []byte(username),
+		xpv1.ResourceCredentialsSecretPasswordKey: []byte(password),
+	}
+}
--- internal/controller/keyspace/keyspace_test.go
+++ internal/controller/keyspace/keyspace_test.go
@@ -1,43 +1,87 @@
-/*
-Copyright 2022 The Crossplane Authors.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package keyspace
 
 import (
 	"context"
+	"github.com/pkg/errors"
 	"testing"
-
-	"github.com/google/go-cmp/cmp"
+	"github.com/gocql/gocql"
 
 	"github.com/crossplane/crossplane-runtime/pkg/reconciler/managed"
 	"github.com/crossplane/crossplane-runtime/pkg/resource"
 	"github.com/crossplane/crossplane-runtime/pkg/test"
+	"github.com/google/go-cmp/cmp"
+
+	"github.com/crossplane/provider-cassandra/apis/cql/v1alpha1"
+	"github.com/crossplane/provider-cassandra/internal/clients/cassandra"
 )
 
-// Unlike many Kubernetes projects Crossplane does not use third party testing
-// libraries, per the common Go test review comments. Crossplane encourages the
-// use of table driven unit tests. The tests of the crossplane-runtime project
-// are representative of the testing style Crossplane encourages.
-//
-// https://github.com/golang/go/wiki/TestComments
-// https://github.com/crossplane/crossplane/blob/master/CONTRIBUTING.md#contributing-code
+func TestConnect(t *testing.T) {
+	errBoom := errors.New("boom")
+
+	type fields struct {
+			kube      resource.ClientApplicator
+			usage     resource.Tracker
+			newClient func(creds map[string][]byte, keyspace string) cassandra.DB
+		}
+
+	type args struct {
+			ctx context.Context
+			mg  resource.Managed
+		}
+
+	type want struct {
+			client managed.ExternalClient
+			err    error
+		}
+
+	cases := map[string]struct {
+		reason string
+		fields fields
+		args   args
+		want   want
+	}{
+		"ErrNotKeyspace": {
+			reason: "Should return an error when the managed resource is not a *Keyspace",
+			args: args{
+				mg: nil,
+			},
+			want: want{
+				err: errors.New(errNotKeyspace),
+			},
+		},
+		"ErrTrackPCUsage": {
+			reason: "Should return an error when tracking provider config usage fails",
+			fields: fields{
+				usage: resource.TrackerFn(func(ctx context.Context, mg resource.Managed) error { return errBoom }),
+			},
+			args: args{
+				mg: &v1alpha1.Keyspace{},
+			},
+			want: want{
+				err: errors.Wrap(errBoom, errTrackPCUsage),
+			},
+		},
+	}
+
+	for name, tc := range cases {
+		t.Run(name, func(t *testing.T) {
+			c := &connector{
+				kube:      tc.fields.kube,
+				usage:     tc.fields.usage,
+				newClient: tc.fields.newClient,
+			}
+			_, err := c.Connect(tc.args.ctx, tc.args.mg)
+			if diff := cmp.Diff(tc.want.err, err, test.EquateErrors()); diff != "" {
+				t.Errorf("\n%s\nConnect(...): -want error, +got error:\n%s\n", tc.reason, diff)
+			}
+		})
+	}
+}
 
 func TestObserve(t *testing.T) {
+
 	type fields struct {
-		service interface{}
+		db cassandra.DB
 	}
 
 	type args struct {
@@ -56,19 +100,48 @@ func TestObserve(t *testing.T) {
 		args   args
 		want   want
 	}{
-		// TODO: Add test cases.
+		"ErrNotKeyspace": {
+			reason: "Should return an error if the managed resource is not a *Keyspace",
+			args: args{
+				mg: nil,
+			},
+			want: want{
+				err: errors.New(errNotKeyspace),
+			},
+		},
+		"KeyspaceNotFound": {
+			reason: "Should return ResourceExists: false when the keyspace does not exist",
+			fields: fields{
+				db: &cassandra.MockDB{
+					QueryFunc: func(ctx context.Context, query string, args ...interface{}) (*gocql.Iter, error) {
+						return nil, nil
+					},
+					ScanFunc: func(iter *gocql.Iter, dest ...interface{}) bool { return false },
+				},
+			},
+			args: args{
+				mg: &v1alpha1.Keyspace{},
+			},
+			want: want{
+				o: managed.ExternalObservation{
+					ResourceExists: false,
+				},
+			},
+		},
 	}
 
 	for name, tc := range cases {
 		t.Run(name, func(t *testing.T) {
-			e := external{service: tc.fields.service}
+			e := external{db: tc.fields.db}
 			got, err := e.Observe(tc.args.ctx, tc.args.mg)
 			if diff := cmp.Diff(tc.want.err, err, test.EquateErrors()); diff != "" {
-				t.Errorf("\n%s\ne.Observe(...): -want error, +got error:\n%s\n", tc.reason, diff)
+				t.Errorf("\n%s\nObserve(...): -want error, +got error:\n%s\n", tc.reason, diff)
 			}
 			if diff := cmp.Diff(tc.want.o, got); diff != "" {
-				t.Errorf("\n%s\ne.Observe(...): -want, +got:\n%s\n", tc.reason, diff)
+				t.Errorf("\n%s\nObserve(...): -want, +got:\n%s\n", tc.reason, diff)
 			}
 		})
 	}
 }
+
+// Additional test functions (Create, Update, Delete) can be written following a similar structure.
